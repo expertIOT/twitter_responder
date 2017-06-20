e6ee40d0453354d72f3444b699ed39ea8aa38e8d
Andrew Li <andrew.li1987@yandex.com>
2017-06-20 17:24:20 +0900

    auto responder

---
+++ TwitterMsgServices/CommonProvider.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using TweetSharp;
+
+namespace TwitterMsgServices
+{
+    public class TwitAuthenticateResponse
+    {
+        public string token_type { get; set; }
+        public string access_token { get; set; }
+    }
+
+    class CommonProvider
+    {
+        public static string connString = "xxx";
+
+        public static string []consumer_key = { "xxx", "xxx" };
+        public static string []consumer_seckey = { "xx", "xxx" };
+        public static string []access_token = { "xxx", "xx" };
+        public static string []access_sectoken = { "xxx", "xxx" };
+        public static long[] userids = {111, 111 };
+        public static void WriteErrorLog(string error, Exception ex = null)
+        {
+            StreamWriter sw = null;
+            if (ex == null) ex = new Exception();
+            try
+            {
+                sw = new StreamWriter("C:\\inetpub\\wwwroot\\log.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + error);
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+
+            }
+        }
+        public static int getScalarValueFromDB(string proc_name, List<SqlParameter> proc_param)
+        {
+            int ret = 0;
+            //  adapter.Fill(customers, "Customers");
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connString))
+                {
+                    con.Open();
+                    string sql = proc_name;
+
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    foreach (SqlParameter proc_par in proc_param)
+                    {
+                        cmd.Parameters.Add(proc_par);
+                    }
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Int32.TryParse(reader[0].ToString(), out ret);
+                        break;
+                    }
+                    con.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return ret;
+
+        }
+        public static DataSet getDataSet(string proc_name, List<SqlParameter> proc_param)
+        {
+
+            //  SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+
+            DataSet inquiry_set = new DataSet();
+            //  adapter.Fill(customers, "Customers");
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connString))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
+                    {
+                        con.Open();
+                        string sql = proc_name;
+
+                        SqlCommand cmd = new SqlCommand(sql, con);
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        foreach (SqlParameter proc_par in proc_param)
+                        {
+                            cmd.Parameters.Add(proc_par);
+                        }
+
+
+                        adapter.SelectCommand = cmd;
+
+                        adapter.Fill(inquiry_set, "Result");
+
+                        con.Close();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return inquiry_set;
+        }
+        public static List<long> getFollowerIDs(int index=0, string next_cursor="")
+        {
+
+            // JObject result_ids = null;
+            /*   public static JObject getFollowerIDs(string next_cursor ,int index=0)
+             *           try
+                       {
+
+
+                           var oauth_consumer_key = consumer_key;
+                           var oauth_consumer_secret = consumer_seckey;
+                           //Token URL
+                           var oauth_url = "https://api.twitter.com/oauth2/token";
+                           var headerFormat = "Basic {0}";
+                           var authHeader = string.Format(headerFormat,
+                           Convert.ToBase64String(Encoding.UTF8.GetBytes(Uri.EscapeDataString(oauth_consumer_key[index]) + ":" +
+                           Uri.EscapeDataString((oauth_consumer_secret[index])))
+                           ));
+
+                           var postBody = "grant_type=client_credentials";
+
+                           ServicePointManager.Expect100Continue = false;
+                           HttpWebRequest request = (HttpWebRequest)WebRequest.Create(oauth_url);
+                           request.Headers.Add("Authorization", authHeader);
+                           request.Method = "POST";
+                           request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+
+                           using (Stream stream = request.GetRequestStream())
+                           {
+                               byte[] content = ASCIIEncoding.ASCII.GetBytes(postBody);
+                               stream.Write(content, 0, content.Length);
+                           }
+
+                           request.Headers.Add("Accept-Encoding", "gzip");
+                           HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                           Stream responseStream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);
+                           JObject result_token;
+                           TwitAuthenticateResponse twitAuthResponse;
+                           using (var reader = new StreamReader(responseStream))
+                           {
+                               // JavaScriptSerializer js = new JavaScriptSerializer();
+                               var objText = reader.ReadToEnd();
+                               // result_token = JObject.Parse(objText);
+                               twitAuthResponse = JsonConvert.DeserializeObject<TwitAuthenticateResponse>(objText);
+                           }
+
+                           //Get the follower ids
+
+                           var url = "https://api.twitter.com/1.1/followers/ids.json?cursor=" + next_cursor + "&screen_name=vacationsabroad&skip_status=true&include_user_entities=false";
+                           //var url = "https://api.twitter.com/1.1/direct_messages/new.json?text=hello%2C%20tworld.%20welcome%20to%201.1.&screen_name=andrew_lidev";
+
+                           HttpWebRequest timeLineRequest = (HttpWebRequest)WebRequest.Create(url);
+                           var timelineHeaderFormat = "{0} {1}";
+                           timeLineRequest.Headers.Add("Authorization", string.Format(timelineHeaderFormat, twitAuthResponse.token_type, twitAuthResponse.access_token));
+                           timeLineRequest.Method = "GET";
+                           WebResponse timeLineResponse = timeLineRequest.GetResponse();
+                           var timeLineJson = string.Empty;
+
+
+
+                           using (timeLineResponse)
+                           {
+                               using (var reader = new StreamReader(timeLineResponse.GetResponseStream()))
+                               {
+                                   timeLineJson = reader.ReadToEnd();
+                                   CommonProvider.WriteErrorLog(timeLineJson);
+                                   result_ids = JObject.Parse(timeLineJson);
+
+                               }
+                           }
+                       }
+                       catch(Exception ex)
+                       {
+                           CommonProvider.WriteErrorLog(index +"===>" + ex.Message + " :Source Message" +ex.Source);
+                       }
+                       return result_ids;
+                    */
+            List<long> lstFollowers = new List<long>();
+            try
+            {
+                var service = new TwitterService(consumer_key[index], consumer_seckey[index]);
+                service.AuthenticateWith(access_token[index], access_sectoken[index]);
+
+                long t_userid = userids[index];
+
+                try
+                {
+                    TwitterUser tuSelf = service.GetUserProfile(
+                    new GetUserProfileOptions() { IncludeEntities = false, SkipStatus = false });
+                    t_userid = tuSelf.Id;
+                }catch(Exception ex)
+                {
+                    WriteErrorLog(String.Format("{0}==>Used instead of userid. Get Profile Error;{1} source:{2} stack:{3}",index, ex.Message, ex.Source, ex.StackTrace));
+                }
+
+                //Console.WriteLine(String.Format("{0} {1} {2}", tuSelf.Id, tuSelf.ScreenName, tuSelf.FollowersCount));
+                // return;
+                // var options = new ListFollowersOptions { ScreenName = tuSelf.ScreenName };
+                /*        ListFollowersOptions options = new ListFollowersOptions();
+                             //   options.UserId = tuSelf.Id;
+                                options.ScreenName = tuSelf.ScreenName;
+                                options.IncludeUserEntities = false;
+                                options.SkipStatus = true;
+                                options.Cursor = -1;
+
+                                List<TwitterUser> lstFollowers = new List<TwitterUser>();
+                              TwitterCursorList<TwitterUser> followers = service.ListFollowers(options);
+            */
+
+                ListFollowerIdsOfOptions options = new ListFollowerIdsOfOptions();
+                options.Cursor = -1;
+                options.Count = 3000;
+                options.UserId = t_userid;
+
+                // if the API call did not succeed
+
+                while (true)
+                {
+                    TwitterCursorList<long> followers = service.ListFollowerIdsOf(options);
+                    //If the followers exists
+                    if (followers == null)
+                    {
+                        WriteErrorLog(index + "===> there is no followers !! error");
+                        break;
+                    }
+                    else
+                    {
+                        foreach (long user in followers)
+                        {
+                            // do something with the user (I'm adding them to a List)
+                            lstFollowers.Add(user);
+                        }
+                    }
+
+                    // if there are more followers
+                    if (followers.NextCursor != null &&
+                        followers.NextCursor != 0)
+                    {
+                        // then advance the cursor and load the next page of results
+                        options.Cursor = followers.NextCursor;
+                        followers = service.ListFollowerIdsOf(options);
+                    }
+                    // otherwise, we're done!
+                    else
+                        break;
+                } 
+
+            }catch(Exception ex)
+            {
+                WriteErrorLog(String.Format("{0}===>Error: {1} Sourc: {2}", index, ex.Message, ex.Source));
+            }
+
+            return lstFollowers;
+        }
+        public static void sendDMMessage(long id, string message, int index=0)
+        {
+            var service = new TwitterService(consumer_key[index], consumer_seckey[index]);
+            service.AuthenticateWith(access_token[index], access_sectoken[index]);
+            service.SendDirectMessage(new SendDirectMessageOptions() { UserId = id, Text = message });
+            // service.ListFollowers(new ListFollowersOptions { Cursor = -1, ScreenName = "vacationsabroad", IncludeUserEntities = false, SkipStatus = true });
+        }
+    }
+}
---
+++ TwitterMsgServices/TwitterMsgService.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using TweetSharp;
+
+namespace TwitterMsgServices
+{
+    public partial class TwitterMsgService : ServiceBase
+    {
+
+        protected static DataSet ds_twitters;
+        protected static List<long> []list_twitterid = { new List<long>(), new List<long>()};
+
+        /* private static void OnTimedEvent(object source, ElapsedEventArgs e)
+         {
+             Console.WriteLine("Hello World!");
+         }
+         */
+         //message content for sending
+        public static string[] message = { @"You Won! 20% Discount on your next vacation.  
+Thanks for the follow! 
+Enter code: twitterfollower at checkout.
+https://www.vacations-abroad.com"
+,@"I know you work hard all year.
+Tag your summer photos #exploretheworldundaunted to enter our $500 giveaway credit towards a Vacations-abroad.com reservation." };
+        public static string[] tblname = { "TwitterFollower", "TwitterUserID"};
+        public static void getFollowers( int index=0, string next_cursor="-1")
+        {
+
+            // JObject result_ids = CommonProvider.getFollowerIDs( index);
+            List<long> result_ids = CommonProvider.getFollowerIDs(index, next_cursor);
+
+            foreach (long row in result_ids)
+            {
+                if (!list_twitterid[index].Contains(row))
+                {
+
+                    //Send welcome message
+                    List<SqlParameter> param = new List<SqlParameter>();
+                    param.Add(new SqlParameter("@twitterid", row));
+                    param.Add(new SqlParameter("@tblname", tblname[index]));
+                    CommonProvider.getDataSet("uspAddTwitterFollowerID", param);
+                    list_twitterid[index].Add(row);
+                    CommonProvider.WriteErrorLog(String.Format("{0}==> New user followed you. ID: {1}", index, row));
+                    try
+                    {
+                        CommonProvider.sendDMMessage(row, message[index], index);
+                    }catch(Exception ex)
+                    {
+                        CommonProvider.WriteErrorLog(String.Format("{0} ==>sending dm message error:  {1} ID:{2} ==>", index, ex.Message, row));
+                    }
+
+                }
+            }
+        }
+        public TwitterMsgService()
+        {
+            InitializeComponent();
+        }
+        //when Service Starting , fires this function
+        protected override void OnStart(string[] args)
+        {
+            CommonProvider.WriteErrorLog("Twilio service started");
+
+            list_twitterid[0].Clear();
+            list_twitterid[1].Clear();
+
+            List<SqlParameter> param = new List<SqlParameter>();
+            for(int i=0; i<2; i++)
+            {
+                param.Clear();
+                param.Add(new SqlParameter("@tblname", tblname[i]));
+                ds_twitters = CommonProvider.getDataSet("uspGetTwitterFollowerIDs", param);
+
+                foreach (DataRow row in ds_twitters.Tables[0].Rows)
+                {
+                    try
+                    {
+                        list_twitterid[i].Add(long.Parse(row[0].ToString()));
+                    }
+                    catch { }
+                }
+            }
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 600000; // 600 seconds  
+           // timer.Interval = 60000; // 600 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            CommonProvider.WriteErrorLog("Twitter service ended");
+        }
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.  
+            for(int i=0;i< 2; i++)
+            {
+                getFollowers( i, "-1");
+            }
+            CommonProvider.WriteErrorLog("Twilio service working!!!");
+        }
+
+    }
+}
